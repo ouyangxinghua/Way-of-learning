5caf172803e1ec9abf0130dbbc25e72bc34b0569
8westbrook <1792984726@qq.com>
2020-03-15 10:24:23 +0800

    dd

--- 前端学习之路/JavaScript学习/调试代码/vue3Proxy.js
+++ 前端学习之路/JavaScript学习/调试代码/vue3Proxy.js
@@ -2,7 +2,8 @@
 // handler：一个对象，定义要拦截的行为
 
 // const p = new Proxy(target, handler);
-
+// https://segmentfault.com/q/1010000021048654
+// https://juejin.im/post/5e69ee2be51d4527196d6a24
 const p = new Proxy({}, function(){
     
 })
